97bce6cbc428e01239cb3ee411725ca5859698eb
Muhammad Agya <Muhammadagya567@gmail.com>
2022-10-21 15:21:27 +0700

    mengirim data kompenen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    Dashboard2Component,
+    Dashboard3Component
   ],
   imports: [
     BrowserModule,
